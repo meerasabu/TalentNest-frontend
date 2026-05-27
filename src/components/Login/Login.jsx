014de76b33e927a362479ab83426651e436c9103
meerasabu <meerasabu9@gmail.com>
2026-05-27 16:58:00 +0530

    corss2

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,9 +7,9 @@ const Login = () => {
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
-    campusEmail: '',
-    password: ''
-  });
+  email: '', 
+  password: ''
+});
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -90,7 +90,12 @@ const Login = () => {
                   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                 </svg>
               </span>
-              <input type="email" id="campusEmail" placeholder="name@university.edu" value={formData.campusEmail} onChange={handleChange} required />
+              <input 
+                type="email" 
+                id="email" 
+                value={formData.email || formData.campusEmail || ''} 
+                onChange={handleChange} 
+                required />
             </div>
           </div>
  
